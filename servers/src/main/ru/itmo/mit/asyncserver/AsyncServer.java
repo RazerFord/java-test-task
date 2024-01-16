1b589634cf1cc23244f44b7b4ecbc483d7e6f968
RazerFord <desk10567@gmail.com>
2024-01-17 01:14:25 +0300

    feat: begin async server

---
+++ servers/src/main/ru/itmo/mit/asyncserver/AsyncServer.java
@@ -0,0 +1,12 @@
+package ru.itmo.mit.asyncserver;
+
+import ru.itmo.mit.Server;
+
+import java.io.IOException;
+
+public class AsyncServer implements Server {
+    @Override
+    public void start() throws IOException {
+
+    }
+}
