64deede0fcb2f4f0b870e880d9d94bad166ab3b0
RazerFord <desk10567@gmail.com>
2024-01-17 19:49:00 +0300

    feat: first implementation client

---
+++ client/src/main/ru/mit/itmo/Client.java
@@ -0,0 +1,117 @@
+package ru.mit.itmo;
+
+import org.jetbrains.annotations.NotNull;
+import ru.itmo.mit.MessageOuterClass;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Client implements Runnable {
+    private static final Logger LOGGER = Logger.getLogger(Client.class.getName());
+    private static final int FACTOR = 2;
+    private static final int INITIAL_BUFFER_SIZE = 1024;
+    private final Random random = new Random();
+    private ByteBuffer buffer = ByteBuffer.allocate(INITIAL_BUFFER_SIZE);
+    private final String targetAddress;
+    private final int targetPort;
+    private final int countNumbers;
+    private final int countRequest;
+    private final int periodRequest;
+
+    public Client(
+            String targetAddress,
+            int targetPort,
+            int countNumbers,
+            int countRequest,
+            int periodRequest
+    ) {
+        this.targetAddress = targetAddress;
+        this.targetPort = targetPort;
+        this.countNumbers = countNumbers;
+        this.countRequest = countRequest;
+        this.periodRequest = periodRequest;
+    }
+
+    @Override
+    public void run() {
+        try (
+                var socket = new Socket(targetAddress, targetPort);
+                var outputStream = socket.getOutputStream();
+                var inputStream = socket.getInputStream()
+        ) {
+            var message = buildRequest();
+            send(message, outputStream);
+            message = read(inputStream);
+            checkSortingList(message.getNumberList());
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING, e.getMessage());
+        }
+    }
+
+    private void send(MessageOuterClass.@NotNull Message message, OutputStream outputStream) throws IOException {
+        final int size = message.getSerializedSize();
+        final int capacity = buffer.capacity();
+        if (capacity < Integer.BYTES + size) increaseBuffer((Integer.BYTES + size + capacity - 1) / capacity);
+        buffer.putInt(size).put(message.toByteArray());
+        outputStream.write(buffer.array());
+        buffer.clear();
+    }
+
+    private MessageOuterClass.@NotNull Message read(InputStream inputStream) throws IOException {
+        int totalRead = 0;
+        while (totalRead < Integer.BYTES) {
+            int read = inputStream.read(buffer.array());
+            if (read == -1) throw END_STREAM;
+            totalRead += read;
+        }
+        buffer.position(totalRead);
+        final var size = buffer.flip().getInt();
+        buffer.compact();
+        totalRead -= Integer.BYTES;
+        while (totalRead < size) {
+            int read = inputStream.read(buffer.array());
+            if (read == -1) throw END_STREAM;
+            totalRead += read;
+        }
+        MessageOuterClass.Message message = MessageOuterClass.Message.parseFrom(buffer);
+        buffer.clear();
+        return message;
+    }
+
+    private MessageOuterClass.@NotNull Message buildRequest() {
+        List<Integer> numbers = new ArrayList<>(countNumbers);
+        for (int i = 0; i < countNumbers; i++) {
+            numbers.add(random.nextInt());
+        }
+        return MessageOuterClass.Message.newBuilder().addAllNumber(numbers).build();
+    }
+
+    private void increaseBuffer() {
+        increaseBuffer(FACTOR);
+    }
+
+    private void increaseBuffer(int factor) {
+        int newSizeBuffer = factor * buffer.capacity();
+        ByteBuffer newByte = ByteBuffer.wrap(Arrays.copyOf(buffer.array(), newSizeBuffer));
+        newByte.position(buffer.position());
+        buffer = newByte;
+    }
+
+    private void checkSortingList(@NotNull List<Integer> numbers) {
+        for (int i = 1; i < numbers.size(); i++) {
+            if (numbers.get(i - 1) > numbers.get(i)) {
+                throw LIST_NOT_SORTED;
+            }
+        }
+    }
+
+    private static final ClientException END_STREAM = new ClientException("End of stream reached");
+    private static final ClientException LIST_NOT_SORTED = new ClientException("The list is not sorted");
+}
--- starter/src/main/ru/itmo/mit/Main.java
+++ starter/src/main/ru/itmo/mit/Main.java
@@ -1,20 +1,17 @@
 package ru.itmo.mit;
 
-import ru.itmo.mit.asyncserver.AsyncServer;
+import ru.itmo.mit.blockingserver.BlockingServer;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
-import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
-        var server = new AsyncServer(8081);
+        var server = new BlockingServer(8081);
         new Thread(() -> {
             try {
                 server.start();
@@ -22,55 +19,9 @@ public class Main {
                 throw new RuntimeException(e);
             }
         }).start();
-        ByteBuffer buffer = ByteBuffer.allocate(12);
-        for (int i = 0; i < 3; i++) {
-            buffer.putInt(i);
-        }
-        buffer.flip();
-        buffer = increaseReadBuffer(buffer);
-
-        System.out.println(buffer.getInt());
-        System.out.println(buffer.getInt());
-        System.out.println(buffer.getInt());
-        int i = 0;
-        try (Socket socket = new Socket("0.0.0.0", 8081);
-             Socket socket1 = new Socket("0.0.0.0", 8081);
-             InputStream inputStream = socket.getInputStream();
-             OutputStream outputStream = socket.getOutputStream()) {
-            while (i++ < 10) {
-                MessageOuterClass.Message message = MessageOuterClass
-                        .Message
-                        .newBuilder()
-                        .addAllNumber(List.of(234, 12, 53, 2, 5, 12, 5, 123, 5))
-                        .build();
-
-                int size = message.getSerializedSize();
-                System.out.println(size);
-
-                outputStream.write(
-                        ByteBuffer.allocate(Integer.BYTES + size)
-                                .putInt(size)
-                                .put(message.toByteArray())
-                                .array()
-                );
-
-                size = Integer.BYTES;
 
 
-                ByteBuffer byteBuffer = ByteBuffer.allocate(size);
-                if (!read(inputStream, byteBuffer, size)) return;
 
-                size = byteBuffer.getInt();
-
-                byteBuffer = ByteBuffer.allocate(size);
-                if (!read(inputStream, byteBuffer, size)) return;
-                message = MessageOuterClass.Message.parseFrom(byteBuffer);
-                for (int f : message.getNumberList()) {
-                    System.out.printf("%s ", f);
-                }
-            }
-        }
-        System.out.println("END");
         try {
             server.close();
         } catch (Exception e) {
