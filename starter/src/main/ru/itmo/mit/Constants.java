9e68371e598cda735c627e7b4a6c7aa93c057887
RazerFord <desk10567@gmail.com>
2024-01-21 12:19:10 +0300

    fix: fixed

--- starter/src/main/ru/itmo/mit/Constants.java
+++ starter/src/main/ru/itmo/mit/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
     public static final String ADDRESS = "0.0.0.0";
     public static final int PORT = 8081;
-    public static final Object[] PARAMS = {"N", "M", "Δ"};
+    public static final Object[] PARAMS = {"N", "M", "D"};
 
     public static final String SELECT_ARCHITECTURE = """
             Select architecture:
@@ -23,7 +23,7 @@ public class Constants {
             Select parameter to change:
             1. N - Number of elements in the array
             2. M - Number of working clients
-            3. Δ - Time interval between requests, ms
+            3. D - Time interval between requests, ms
             """;
 
     public static final String SELECT_STEP_FROM_TO = """
@@ -34,7 +34,7 @@ public class Constants {
             Select other values: %s, %s
             N - Number of elements in the array
             M - Number of working clients
-            Δ - Time interval between requests, ms
+            D - Time interval between requests, ms
             """;
 
     public static final IllegalArgumentException PARAMETER_NOT_NEGATIVE = new IllegalArgumentException("Parameter must not be negative");
